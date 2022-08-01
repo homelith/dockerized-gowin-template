975562f349bd28235818d9cf07a071e03516bc78
homelith <homemoto.toru@gmail.com>
2022-08-01 12:02:30 +0900

    picked xrdp support from main and added 1.9.8.07 support

--- 1.9.8.07/README.md
+++ 1.9.8.07/README.md
@@ -1,9 +1,9 @@
-# template project for gowin-tools:1.9.8.05
+# template project for gowin-tools:1.9.8.07
 
 ## quick start
 
 - get `docker` command introduced and accesible from non-privilege user
-- `$ make docker` and get gowin 1.9.8.05 suitable ubuntu console
+- `$ make docker` and get gowin 1.9.8.07 suitable ubuntu console
 - `$ /root/install.sh` to install tools on /opt/Gowin
 - now you can use `gw_ide` and `programmer_cli` commands
 
---
+++ 1.9.8.07/docker/settings.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+GOWIN_TOOL_VERSION=1.9.8.07
+GOWIN_ROOT_DIR=/opt/Gowin
+
+export QT_X11_NO_MITSHM=1
+if [ -z ${LD_LIBRARY_PATH} ]; then
+	export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:${GOWIN_ROOT_DIR}/${GOWIN_TOOL_VERSION}/IDE/lib
+else
+	export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/lib/x86_64-linux-gnu:${GOWIN_ROOT_DIR}/${GOWIN_TOOL_VERSION}/IDE/lib
+fi
+export PATH=${PATH}:${GOWIN_ROOT_DIR}/${GOWIN_TOOL_VERSION}/IDE/bin
+export PATH=${PATH}:${GOWIN_ROOT_DIR}/${GOWIN_TOOL_VERSION}/Programmer/bin
+echo "---- gowin-tools:${GOWIN_TOOL_VERSION} container shell ----"
+if !(type gw_ide > /dev/null 2>&1); then
+	echo "command 'gw_ide' not found. try '$ /root/install.sh' to install tools on /opt"
+else
+	echo "'gw_ide', 'programmer_cli' and some gowin tools successfully introduced on your PATH"
+fi
+xhost +
